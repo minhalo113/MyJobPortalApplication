91fdbc1e18b645d09130fbe8b82ab1f6da21e379
minhalo113 <115437688+minhalo113@users.noreply.github.com>
2023-07-06 16:10:49 -0600

    yo

---
+++ app/src/main/java/com/example/myjobportalapplication/JobListActivity.java
@@ -0,0 +1,14 @@
+package com.example.myjobportalapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_list);
+    }
+}
